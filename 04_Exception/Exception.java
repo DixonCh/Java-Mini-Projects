96e52f3b8b60ba8eb904bccdae41e38c57164e52
DixonCh <dixonchaudhary7@gmail.com>
2020-04-09 13:55:48 +0545

    Add files via upload

---
+++ 04_Exception/Exception.java
@@ -0,0 +1,20 @@
+public class Exception {
+	public static void main(String[] args) {
+		try {
+			int a, b, c;
+			a = 5;
+			b = 0;
+			c = a / b;
+			System.out.println("This will not be printed.");
+			System.out.println(c);
+		} catch (java.lang.Exception e) {
+			System.out.println("Error is caught.");
+			System.out.println("Divide by zero.");
+
+		} finally {
+			System.out.println("This is finally section.");
+		}
+
+	}
+
+}
