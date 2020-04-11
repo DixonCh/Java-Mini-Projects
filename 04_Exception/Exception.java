69e123827aed27730eff9f5bcc4d3c22572f3214
DixonCh <dixonchaudhary7@gmail.com>
2020-04-11 21:33:25 +0545

    Update Exception.java

--- 04_Exception/Exception.java
+++ 04_Exception/Exception.java
@@ -1,7 +1,9 @@
 //Program to show the exception
 
 public class Exception {
+	
 	public static void main(String[] args) {
+		//try block
 		try {
 			int a, b, c;
 			a = 5;
@@ -9,11 +11,15 @@ public class Exception {
 			c = a / b;
 			System.out.println("This will not be printed.");
 			System.out.println(c);
-		} catch (java.lang.Exception e) {
+		} 
+		//catch block
+		catch (java.lang.Exception e) {
 			System.out.println("Error is caught.");
 			System.out.println("Divide by zero.");
 
-		} finally {
+		} 
+		//finally block
+		finally {
 			System.out.println("This is finally section.");
 		}
 
